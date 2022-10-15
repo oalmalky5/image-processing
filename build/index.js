9bc21be8252b8adfd6974f369e61024805483d4a
oalmalky5 <oalmalky5@gmail.com>
2022-10-15 08:08:49 +0300

    configuring typescript

---
+++ build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var myName = 'Omar';
+var hello = function (userName) { return "hello ".concat(userName); };
+console.log(hello(myName));
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const myName = 'Omar'
+
+const hello = (userName: string): string => `hello ${userName}`
+
+console.log(hello(myName))
