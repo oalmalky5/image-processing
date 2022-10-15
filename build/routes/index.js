ab575c26645906529685e760a558278769465a4c
oalmalky5 <oalmalky5@gmail.com>
2022-10-15 09:13:25 +0300

    adding jasmine and other dependencies

--- build/routes/index.js
+++ build/routes/index.js
@@ -4,10 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var app = (0, express_1.default)();
-var PORT = 3000 || process.env.PORT;
-var index_1 = __importDefault(require("./routes/index"));
-app.use('/api', index_1.default);
-app.listen(PORT, function () {
-    console.log("Server running on port ".concat(PORT));
+var router = express_1.default.Router();
+router.get('/', function (req, res) {
+    res.send('Hello, World!');
 });
+exports.default = router;
